9a45f3faa008af9620c637afcfdb09dd6c846b19
Jessica Morrell <jessica.ann.morrell@gmail.com>
2016-02-11 20:51:28 -0700

    finished paint tutorial

--- app.js
+++ app.js
@@ -1,38 +1,97 @@
 $(document).ready(function(){
 
-	var color = "white";
-		//selector //listener //callback
-	$('.box').on("click", function(){
-		$(this).addClass(color); // function body
+	var box = $('.box');
+	var color = 'white';
+	var colors = 'red blue green yellow white';
+
+	box.mouseenter('click', function() {
+		$(this).addClass(color);
 	})
 
-	$(".box").on('dblclick', function(){
-		$(this).removeClass(color);
+	box.on('dblclick', function() {
+		$(this).removeClass(colors);
 	})
 
-	$('#reset').on('click', function(){
-		$('.box').removeClass('red blue green yellow white');
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
 	})
 
-	$('#red').on('click', function(){
+	$('#red').on('click', function() {
 		color = 'red';
 	})
 
-	$('#blue').on('click', function(){
+	$('#blue').on('click', function() {
 		color = 'blue';
 	})
-
-	$('#green').on('click', function(){
+	$('#green').on('click', function() {
 		color = 'green';
 	})
-
-	$('#yellow').on('click', function(){
+	$('#yellow').on('click', function() {
 		color = 'yellow';
 	})
-
-	$('#white').on('click', function(){
+	$('#white').on('click', function() {
 		color = 'white';
 	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// var color = "white";
+	// 	//selector //listener //callback
+	// $('.box').on("click", function(){
+	// 	$(this).addClass(color); // function body
+	// })
+
+	// $(".box").on('dblclick', function(){
+	// 	$(this).removeClass(color);
+	// })
+
+	// $('#reset').on('click', function(){
+	// 	$('.box').removeClass('red blue green yellow white');
+	// })
+
+	// $('#red').on('click', function(){
+	// 	color = 'red';
+	// })
+
+	// $('#blue').on('click', function(){
+	// 	color = 'blue';
+	// })
+
+	// $('#green').on('click', function(){
+	// 	color = 'green';
+	// })
+
+	// $('#yellow').on('click', function(){
+	// 	color = 'yellow';
+	// })
+
+	// $('#white').on('click', function(){
+	// 	color = 'white';
+	// })
 
 
 
